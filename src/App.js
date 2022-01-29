f22a5ec12d039304c054f6bd6953a1e647b144f8
ArunJain12 <arunjain3395@gmail.com>
2022-01-29 17:57:36 +0530

    State Management By useState Hook

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { CounterButton } from './CounterButton';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>State Management Example</h1>
+      <CounterButton />
+    </div>
+  );
+}
+
+export default App;
